77b0e649e1312901eb03df3953e08f8d7575ab55
Kass <dongduclan27072001@gmail.com>
2023-12-14 20:05:55 +0700

    commit

--- App.js
+++
@@ -1,9 +0,0 @@
-import react from "react"
-import 'react-native-gesture-handler';
-import RootStack from './src/navigators/RootStack'
-
-export default function App() {
-  return (
-    <RootStack />
-  );
-}
